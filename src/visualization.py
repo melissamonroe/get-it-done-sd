b74a9934c3ebea00e8cc16f9c71fe766ddaac51d
Melissa Monroe <everythinggoesnow@gmail.com>
2021-04-12 19:48:57 -0700

    updates

--- src/visualization.py
+++ src/visualization.py
@@ -64,22 +64,6 @@ class Visualizer:
 
         # Drop any rows that have NaN fields
         df.dropna(how ='any', inplace=True) 
-
-        # Remove outliers for sqft
-        # for data_id in df.loc[df['listing_sqft'] == '']['data_id']:
-        #     df.drop(df.index[df['data_id'] == data_id], inplace = True) 
-        
-
-        # Drop any duplicates. Dups can happen when a listing is duplicated by the user
-        # in order to get their listing on the first page. They will have the same data 
-        # except for the data_id 
-        # df.drop_duplicates(subset=[ 'listing_title',
-        #                             'listing_bedbath',
-        #                             'listing_price',
-        #                             'listing_addrzip',
-        #                             'listing_sqft'], 
-        #                             inplace=True,
-        #                             keep='last')
         
         ##################################
         #         END CLEAN DATA         #
@@ -103,42 +87,4 @@ class Visualizer:
            "Count": SR_count   
             })
         return summary_stats_df
-
-    # def create_top20(self):
-    #     summary_stats_df = self.get_summary_stats()
-    #     summary_stats_highpricezips_df = summary_stats_df.sort_values(by=['Average List Price'], ascending=False).head(20)
-
-    #     mean_highprices = []
-    #     locations_highprices = []
-    #     zipcodes_highprices = []
-
-    #     for m in summary_stats_highpricezips_df['Average List Price']:
-    #         mean_highprices.append(m)
-    #     for l in summary_stats_highpricezips_df['Location (Zip)']:
-    #         locations_highprices.append(l)        
-    #     for z in summary_stats_highpricezips_df['Zip Code']:
-    #         zipcodes_highprices.append(z)  
-        
-    #     x_axis = np.arange(0, len(locations_highprices))
-    #     tick_locations = []
-    #     for x in x_axis:
-    #         tick_locations.append(x)
-            
-    #     fig, ax = plt.subplots(figsize=(12,8)) # set the size that you'd like 
-    #     fig.subplots_adjust(bottom=0.4)
-    #     for item in ([ax.title, ax.xaxis.label, ax.yaxis.label] +
-    #                 ax.get_xticklabels() + ax.get_yticklabels()):
-    #         item.set_fontsize(14)
-    #     fig.suptitle('Top 20 Highest Average Rental Listing Price', fontsize=20)
-
-    #     plt.xlabel("Zip Code")
-    #     plt.ylabel("Average Rental Listing Price ($)")
-
-    #     plt.xlim(-0.75, len(locations_highprices)-.25)
-    #     plt.ylim(0, max(mean_highprices) + 1000)
-
-    #     plt.bar(x_axis,mean_highprices, facecolor="#097392", alpha=0.75, align="center")
-    #     plt.xticks(tick_locations, locations_highprices, rotation=90)
-    #     # @TODO Move the file name out. Is depemdent on where it is run from. 
-    #     plt.savefig("../images/fig01_top20averageprice.png")
         
